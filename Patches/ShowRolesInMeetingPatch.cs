d529fcb7e1a33e00f2e7573a1f6dc00ba68d17ef
yanpla <yannick@plantenga.com>
2024-10-07 00:01:16 +0200

    See roles in meeting

---
+++ Patches/ShowRolesInMeetingPatch.cs
@@ -0,0 +1,24 @@
+using HarmonyLib;
+using UnityEngine;
+
+namespace yanplaRoles.Patches;
+
+[HarmonyPatch(typeof(MeetingHud), nameof(MeetingHud.Update))]
+public static class ShowRolesInMeetingPatch
+{
+    public static void Postfix(MeetingHud __instance)
+    {
+        var player = PlayerControl.LocalPlayer;
+
+        foreach (PlayerVoteArea playerVoteArea in __instance.playerStates)
+        {
+            var targetPlayer = Utils.PlayerById(playerVoteArea.TargetPlayerId);
+            playerVoteArea.ColorBlindName.transform.localPosition = new Vector3(-0.93f, -0.2f, -0.1f);
+            if (playerVoteArea.TargetPlayerId == player.PlayerId && !player.Data.IsDead || player.Data.IsDead && !targetPlayer.Data.IsDead)
+            {
+                playerVoteArea.NameText.color = targetPlayer.Data.Role.NameColor;
+                playerVoteArea.NameText.text = targetPlayer.name + "\n" + targetPlayer.Data.Role.NiceName;
+            }
+        }
+    }
+}
