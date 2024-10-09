fff3e1eb9d5291fbf306d56ecb629f54ee56ff90
yanpla <yannick@plantenga.com>
2024-10-09 19:39:23 +0200

    Remove unused settings

--- Options/Roles/JanitorOptions.cs
+++ Options/Roles/JanitorOptions.cs
@@ -15,10 +15,4 @@ public class JanitorOptions : AbstractOptionGroup
     public override Type AdvancedRole => typeof(Janitor);
 
     public ModdedToggleOption JanitorCanVent { get; } = new("Can Vent", true);
-    
-    [ModdedNumberOption("Clean Cooldown", 5, 60, 2.5f, MiraNumberSuffixes.Seconds)]
-    public float CleanCooldown { get; set; } = 30;
-
-    [ModdedNumberOption("Clean Uses", 0, 10, 1f)]
-    public float CleanUses { get; set; } = 0;
 }
