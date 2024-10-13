b26cdb37a69a753851a9eba997483f98cfb6cb1a
yanpla <yannick@plantenga.com>
2024-10-13 22:04:49 +0200

    fix showrolesmeeting

--- Patches/ShowRolesInMeetingPatch.cs
+++ Patches/ShowRolesInMeetingPatch.cs
@@ -13,18 +13,13 @@ public static class ShowRolesInMeetingPatch
         foreach (PlayerVoteArea playerVoteArea in __instance.playerStates)
         {
             var targetPlayer = Utils.PlayerById(playerVoteArea.TargetPlayerId);
+            var role = targetPlayer.Data.IsDead ? Utils.GetPlayerLastRole(targetPlayer.PlayerId) : targetPlayer.Data.Role;
             playerVoteArea.ColorBlindName.transform.localPosition = new Vector3(-0.93f, -0.2f, -0.1f);
 
-            if (playerVoteArea.TargetPlayerId == player.PlayerId && !player.Data.IsDead || player.Data.IsDead && !targetPlayer.Data.IsDead)
+            if (playerVoteArea.TargetPlayerId == player.PlayerId && !player.Data.IsDead || player.Data.IsDead)
             {
-            playerVoteArea.NameText.color = targetPlayer.Data.Role.NameColor;
-            playerVoteArea.NameText.text = targetPlayer.name + "\n" + targetPlayer.Data.Role.NiceName;
-            }
-            else if (player.Data.IsDead && targetPlayer.Data.IsDead) 
-            {
-            var role = Utils.GetPlayerLastRole(targetPlayer.PlayerId);
             playerVoteArea.NameText.color = role.NameColor;
-            playerVoteArea.NameText.text = targetPlayer.name + "\n" + role.NiceName;
+            playerVoteArea.NameText.text = targetPlayer.Data.PlayerName + "\n" + role.NiceName;
             }
         }
     }
