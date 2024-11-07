3cdd90ec3d0a8bef2661a06aaa4d42885778814d
yanpla <yannick@plantenga.com>
2024-11-07 14:55:25 +0100

    bumped version

--- yanplaRolesPlugin.cs
+++ yanplaRolesPlugin.cs
@@ -17,7 +17,7 @@ namespace yanplaRoles;
 public partial class YanplaRolesPlugin : BasePlugin, IMiraPlugin
 {
     public Harmony Harmony { get; } = new(Id);
-    public const string VersionString = "0.1.6";
+    public const string VersionString = "0.1.7";
     public string OptionsTitleText => "yanplaRoles";
     public ConfigFile GetConfigFile() => Config;
     public override void Load()
